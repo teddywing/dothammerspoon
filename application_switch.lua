c1b42e6e643d77a8d8e98157b4f9c48aa5451ae6
Teddy Wing <git@teddywing.com>
2019-06-19 21:50:46 +0200

    application_switch: Add shortcuts for more frequently used applications
    Hide these under a mode switch as they're not as frequently used as the
    others defined at the top of the file.

--- application_switch.lua
+++ application_switch.lua
@@ -15,3 +15,29 @@ end)
 hs.hotkey.bind({}, 'f4', function()
 	hs.application.find('com.microsoft.rdc.osx.beta'):activate()
 end)
+
+-- Mode to activate lesser-used applications
+application_switch_mode = hs.hotkey.modal.new({}, 'f13')
+
+application_switch_mode:bind({}, 'l', function()
+	hs.application.find('org.libreoffice.script'):activate(activateAllWindows)
+
+	application_switch_mode:exit()
+end)
+
+application_switch_mode:bind({}, 'w', function()
+	hs.application.find('com.microsoft.Word'):activate(activateAllWindows)
+
+	application_switch_mode:exit()
+end)
+
+application_switch_mode:bind({}, 't', function()
+	hs.application.find('com.googlecode.iterm2'):activate(activateAllWindows)
+
+	application_switch_mode:exit()
+end)
+
+-- TODO: Don't do that with a mode, just a list of keys, as the mode sometimes
+-- doesn't activate quick enough
+
+-- TODO: If we stick with F13, maybe bind keys under the left hand to make it usable without looking
