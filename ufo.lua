16be80dc3608907223815c5d95bb59e2d3fc046f
Teddy Wing <git@teddywing.com>
2019-08-12 22:02:47 +0200

    Add missing license headers

--- ufo.lua
+++ ufo.lua
@@ -1,3 +1,19 @@
+-- Copyright (c) 2019  Teddy Wing
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 ufo_mode = hs.hotkey.modal.new({'ctrl', 'option'}, 'u', 'UFO')
 ufo_mode:bind({'ctrl', 'option'}, 'u', 'UFO Off', function()
 	ufo_mode:exit()
