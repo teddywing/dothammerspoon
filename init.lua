9813a0cff50d7e8d6dbd7ce8afe0d466ce84a0c0
Teddy Wing <git@teddywing.com>
2021-03-07 19:38:46 +0100

    init: Remove F8 key binding
    This binding is no longer needed because I've replaced screen colour
    inversion with
    https://github.com/teddywing/Re-Good-Catalina-Invert-Colours, which
    includes built-in support for the shortcut.
    
    Not sure about the monitor check as I haven't been able to test it. If I
    still need to correct for that, I can add it back in later.

--- init.lua
+++ init.lua
@@ -27,16 +27,3 @@ require('window_layout')
 
 hs.loadSpoon('WindowMode')
 spoon.WindowMode:bindHotkeys({ mode = {{'ctrl', 'option'}, 'w'} })
-
-
--- Shortcut to invert screen colours
-hs.hotkey.bind({}, 'f8', function()
-	hs.eventtap.keyStroke({'cmd', 'option', 'ctrl'}, '8')
-
-	-- Counteract a Catalina bug where sometimes inverting screen colours with
-	-- an external monitor connected makes the internal monitor's brightness
-	-- change to 100%.
-	if hs.screen.primaryScreen():name() == 'G247HL' then
-		cbb_brightness_restore()
-	end
-end)
