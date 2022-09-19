bd3f82c4731827ed4687660a1c9b7440ed8b6a26
Teddy Wing <git@teddywing.com>
2022-09-19 20:14:46 +0200

    keyboard_layout: Update copyright

--- keyboard_layout.lua
+++ keyboard_layout.lua
@@ -1,4 +1,4 @@
--- Copyright (c) 2021  Teddy Wing
+-- Copyright (c) 2021–2022  Teddy Wing
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
